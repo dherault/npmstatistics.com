07ae4e2aa27e004958ec5bde08c801835ee213a5
David Hérault <dherault@gmail.com>
2019-05-10 15:18:12 +0200

    Add devServer and API fetcher

--- src/state/reducers/packages.js
+++ src/state/reducers/packages.js
@@ -1,6 +1,15 @@
 function packages(state = {}, action) {
 
-  return state
+  switch (action.type) {
+    case 'CREATE_PACKAGE':
+      return {
+        ...state,
+        [action.payload.id]: action.payload,
+      }
+
+    default:
+      return state
+  }
 }
 
 export default packages
